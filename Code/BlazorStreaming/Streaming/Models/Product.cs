ccb795c4474bc20d133a428823c12287d7075f44
Philip Japikse <skimedic@gmail.com>
2023-12-07 13:36:02 -0500

    added streaming sample

---
+++ Code/BlazorStreaming/Streaming/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Streaming.Models;
+
+public class Product
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public decimal Price { get; set; }
+}
